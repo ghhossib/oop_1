c5eff86fbeaf0aa6faef71a641ff2dccf23f5035
ghhossib <memngr40@gmail.com>
2024-09-18 10:39:21 +0500

    Update агрегация.py

--- агрегация.py
+++ агрегация.py
@@ -17,13 +17,34 @@ class Worker:
     def getSummOfYearForWorker(self):
         return f"Зарплата вместе с премией у сотрудника {self.name} составляет {self.bonus + self.pay.getSummOfYear}"
 
+# класс композиции 
+class Tax:
+    def __init__(self,pay,bonus, tax, name_tax):
+        self.pay = pay
+        self.bonus = bonus
+        self.name_tax = name_tax
+        self.tax = tax
+        self.salary = Salary = (self.pay) #атрибут self.salary явялется объектом класса Salary
+
+    #метод вывода налога 
+    @property
+    def getTaxOfYear(self):
+        return int(self.bonus + self.salary.getSummOfYear) * self.name_tax / 100
+
+    #метод 
+    def getInformationOfTax(self):
+        return f"Сумма налога {self.name_tax} за год составила {self.getTaxOfYear} рублей"
+        
+        
+        
+            
+
 # объект - зарплата за месяц 
 salary_of_month = Salary(20000)
 print(salary_of_month.getSummOfYear)
 # объект - работник 
 sasha = Worker(salary_of_month,20000,'Саша')
 print(sasha.getSummOfYearForWorker())
+tax = Tax(salary_of_month, sasha.bonus,13,'подоходный для физ.лиц')
+print(tax.getInformationOfTax)
             
-
-
-
